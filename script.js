f9c1a8ad8c94dfdb6fb0375065aa34377c296f18
Rezmii <bartoszrezmer20@gmail.com>
2024-02-28 18:37:02 +0100

    added progress bar

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const dayCounterElement = document.querySelector(".day-counter");
 const resetButton = document.querySelector(".reset-button");
 const incButton = document.querySelector(".increment-button");
+const progressElement = document.querySelector(".progress");
+const progressStatsElement = document.querySelector(".progress-stats");
 let dayCount = localStorage.getItem("days");
 
-dayCount === null ? 0 : displayDays(dayCount);
+if (dayCount !== null) {
+  displayStats();
+} else dayCount = 0;
 
 resetButton.addEventListener("click", () => {
   localStorage.removeItem("days");
   dayCount = 0;
-  console.log(dayCount);
-  displayDays(dayCount);
+  displayStats();
 });
 
 incButton.addEventListener("click", changeDayCounter);
@@ -20,7 +23,7 @@ function displayDays(dayCount) {
 
 function changeDayCounter() {
   dayCount++;
-  displayDays(dayCount);
+  displayStats();
   saveToLocalStorage(dayCount);
 }
 
@@ -28,4 +31,17 @@ function saveToLocalStorage(days) {
   localStorage.setItem("days", days);
 }
 
-console.log(dayCount);
+function displayProgress(progressCount) {
+  let percentProgres;
+  if (progressCount <= 100) {
+    percentProgres = progressCount + "%";
+    console.log(percentProgres);
+    progressElement.style.width = percentProgres;
+    progressStatsElement.textContent = `${progressCount}/100`;
+  }
+}
+
+function displayStats() {
+  displayDays(dayCount);
+  displayProgress(dayCount);
+}
